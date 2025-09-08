02ae5993db89a8af9e558271b02b3c4011dd4ccb
Omar Mattos <omarmatt84@gmail.com>
2025-09-08 17:42:05 -0300

    agregamos los archivos de configuración del servidor

---
+++ index.js
@@ -0,0 +1,5 @@
+import Server from "./server/config";
+
+const server = Server();
+
+server.listen()
---
+++ server/config.js
@@ -0,0 +1,31 @@
+import express from "express";
+import cors from "cors";
+import morgan from "morgan";
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+import mongoose from "../db/config.js";
+
+export default class Server {
+    constructor(){
+        this.app = express();
+        this.port = process.env.PORT || 3000;
+        this.middlewares();
+    }
+
+    middlewares(){
+        this.app.use(cors());
+        this.app.use(express.json());
+        this.app.use(morgan("dev"));
+        const __dirname = dirname(fileURLToPath(import.meta.url));
+        this.app.use(express.static(`${__dirname}/../public`));
+        console.log(`${__dirname}/../public`);
+    }
+
+    listen(){
+        this.app.listen(this.port, () => {
+            console.info(
+                `El servidor se está ejecutando en: http://localhost:${this.port}`
+            );
+        });
+    }
+}
