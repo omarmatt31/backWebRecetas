729cb343bc1eb2ee192040800edca5ef4adf0d99
Omar Mattos <omarmatt84@gmail.com>
2025-09-13 11:44:32 -0300

    agregamos la funcion de login para verificar el mail y password

--- src/controllers/usuarios.controllers.js
+++ src/controllers/usuarios.controllers.js
@@ -1,5 +1,6 @@
 import Usuario from "../models/usuario.js";
-import bcrypt from "bcrypt"
+import bcrypt from "bcrypt";
+import generarJWT from "../helpers/generarJWT.js";
 
 export const leerUsuarios = async (req, res) => {
   try {
@@ -13,10 +14,14 @@ export const leerUsuarios = async (req, res) => {
 
 export const crearUsuario = async (req, res) => {
   try {
-    const {nombreUsuario, email, password} = req.body
-    const saltos = bcrypt.genSaltSync(10)
-    const passwordHash = bcrypt.hashSync(password, saltos)
-    const nuevoUsuario = new Usuario({nombreUsuario, email, password: passwordHash});
+    const { nombreUsuario, email, password } = req.body;
+    const saltos = bcrypt.genSaltSync(10);
+    const passwordHash = bcrypt.hashSync(password, saltos);
+    const nuevoUsuario = new Usuario({
+      nombreUsuario,
+      email,
+      password: passwordHash,
+    });
     await nuevoUsuario.save();
     res.status(201).json({
       mensaje: `El usuario: ${nuevoUsuario.nombreUsuario} fue registrado exitosamente`,
@@ -27,3 +32,25 @@ export const crearUsuario = async (req, res) => {
   }
 };
 
+export const login = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    const usuarioExistente = await Usuario.findOne({ email });
+    if (!usuarioExistente) {
+      return res.status(404).json({ mensaje: "No se encontro el usuario" });
+    }
+    const passwordVerificado = bcrypt.compareSync(password, usuarioExistente.password);
+    if (!passwordVerificado) {
+      return res.status(401).json({ mensaje: "Credenciales incorrectas" });
+    }
+    const token = await generarJWT(usuarioExistente.nombreUsuario, usuarioExistente.email);
+    res.status(200).json({
+        mensaje: "Login exitoso",
+        nombreUsuario: usuarioExistente.nombreUsuario,
+        token: token,
+      });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ mensaje: "Error al loguear el usuario" });
+  }
+};
--- src/routes/usuarios.routes.js
+++ src/routes/usuarios.routes.js
@@ -4,8 +4,9 @@ import {
   crearUsuario,
 } from "../controllers/usuarios.controllers.js";
 import validacionUsuario from "../middleware/validarUsuario.js";
+import { login } from "../controllers/usuarios.controllers.js";
 
 const router = Router();
-router.route("/").get(leerUsuarios).post(crearUsuario);
-//router.route("/login").post(login)
+router.route("/").get(leerUsuarios).post(validacionUsuario, crearUsuario);
+router.route("/login").post(login)
 export default router;
