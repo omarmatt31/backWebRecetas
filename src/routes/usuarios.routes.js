03b823607d21f6b66c5c5312ce82c6f39e20203a
Omar Mattos <omarmatt84@gmail.com>
2025-09-15 17:58:19 -0300

    agregamos el middleware verificarJWT para crear, borrar o editar un usuario

--- src/routes/usuarios.routes.js
+++ src/routes/usuarios.routes.js
@@ -8,9 +8,10 @@ import { login } from "../controllers/usuarios.controllers.js";
 import { leerUsuarioPorId } from "../controllers/usuarios.controllers.js";
 import { borrarUsuarioPorId } from "../controllers/usuarios.controllers.js";
 import { editarUsuarioPorId } from "../controllers/usuarios.controllers.js";
+import verificarJWT from "../middleware/verificarJWT.js";
 
 const router = Router();
-router.route("/").get(leerUsuarios).post(validacionUsuario, crearUsuario);
+router.route("/").get(leerUsuarios).post([verificarJWT, validacionUsuario], crearUsuario);
 router.route("/login").post(login)
-router.route('/:id').get(leerUsuarioPorId).delete(borrarUsuarioPorId).put(validacionUsuario, editarUsuarioPorId)
+router.route('/:id').get(leerUsuarioPorId).delete(verificarJWT, borrarUsuarioPorId).put([verificarJWT, validacionUsuario], editarUsuarioPorId)
 export default router;
